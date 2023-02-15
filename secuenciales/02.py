64151c4af5facf11fa042818c57e6fb35784e02c
sebas1726 <sebastianquispecamavilca17@gmail.com>
2023-02-15 12:34:19 -0500

    tarea 3

---
+++ secuenciales/02.py
@@ -0,0 +1,9 @@
+metros = int (input("metros :"))
+centimetros= metros*100.0
+pulgadas= centimetros/2.54
+pies= pulgadas/12
+yardas= pies/3
+print(F"centimetros ={format(centimetros,'.2f')}%")
+print(f"pulgadas ={format(pulgadas,'.2f')}%")
+print(f"pies ={format(pies,'.2f')}%")
+print(f"yardas ={format(yardas,'.2f')}%")
---
+++ secuenciales/03.py
@@ -0,0 +1,5 @@
+millas = int (input("millas :"))
+metros= millas*1609.0
+pies= metros/3.2808
+print(f"metros ={format(metros,'.2f')}%")
+print(f"pies ={format(pies,'.2f')}%")
